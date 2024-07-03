fffbe13c56d291bb433850a6e1bf3efec83f8efe
ThayMendes <thay.gm@live.com>
2024-07-03 14:48:12 -0300

    Nuxt Config

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  alias: {
+    css:"~/assets/css",
+  },
+  css:[
+    "~/assets/css/main.css"],
+  // runtimeConfig: {
+  //   scriptSchoolApiSecret: '-----',
+  //   public: {
+  //     scriptSchoolApiBase: '/nomeAPI',
+  //   }
+  }
+})
